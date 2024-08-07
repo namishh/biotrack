7f6d462a335f7dabd381ad0d4445eb8539f096dc
nam <namish@tuta.io>
2024-08-07 19:14:20 +0530

    do not commit app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ["./views/**/*.{templ}"],
+  content: ["./views/**/*.templ"],
   theme: {
     extend: {
       ...colors,
